e1d098c910defa65a8aec3af35bdeccc9d8c2c70
Brandon Mejia <67741717+brandondev13@users.noreply.github.com>
2023-01-15 12:41:14 -0500

    Delete backReto directory

--- backReto/src/main/java/com/example/quileia/repository/interfaces/ConexionDeRedInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.quileia.repository.interfaces;
-
-import com.example.quileia.model.ConexionDeRed;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ConexionDeRedInterface extends CrudRepository<ConexionDeRed, Integer> {
-
-}
-
